af5cc85fef1c5fdc75246598b7f6beedaa677e80
Kyrylo Chebotarov <kparusik@gmail.com>
2020-06-11 22:54:23 +0300

    Implement MongoDB driver

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,6 +8,21 @@ if (process.env.NODE_ENV === 'test') {
 }
 
 const configSchema = {
+    DB_MONGO_CONNECTION_URI: {
+        default: null,
+        env: 'DB_MONGO_CONNECTION_URI',
+        format: String,
+    },
+    DB_MONGO_HOST: {
+        default: null,
+        env: 'DB_MONGO_HOST',
+        format: String,
+    },
+    DB_MONGO_PORT: {
+        default: null,
+        env: 'DB_MONGO_PORT',
+        format: 'nat',
+    },
     LOGS_LEVEL: {
         default: 'silly',
         env: 'LOGS_LEVEL',
@@ -26,6 +41,9 @@ const configSchema = {
 };
 
 export type ConfigSchema = {
+    DB_MONGO_CONNECTION_URI: string;
+    DB_MONGO_HOST: string;
+    DB_MONGO_PORT: number;
     LOGS_LEVEL: string;
     SERVER_PORT: number;
     SESSION_SECRET: string;
---
+++ src/utils/mongo.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+import waitPort from 'wait-port';
+
+import { log } from './logger';
+import { config } from '../config';
+
+const connectURI = config.get('DB_MONGO_CONNECTION_URI');
+const host = config.get('DB_MONGO_HOST');
+const port = config.get('DB_MONGO_PORT');
+
+mongoose.set('useCreateIndex', true);
+mongoose.Promise = Promise;
+mongoose.connection.on('connecting', () => log.debug('Trying to connect to MongoDB: %s', connectURI));
+mongoose.connection.on('connected', () => log.debug('The connection established to MongoDB: %s', connectURI));
+mongoose.connection.on('disconnected', () => log.debug('Disconnected from MongoDB: %s', connectURI));
+mongoose.connection.on('reconnected', () => log.debug('Reconnected to MongoDB: %s', connectURI));
+
+export const connect = async (): Promise<mongoose.Mongoose> => {
+    try {
+        new Promise((res) => waitPort({ host, port, timeout: 5000 }).then(res));
+
+        const connection = await mongoose.connect(connectURI, {
+            autoIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            reconnectTries: 5,
+        });
+
+        log.info(`Connected to MongoDB: ${connectURI}`);
+        return connection;
+    } catch (e) {
+        log.error('Unable to connect to MongoDB: %s', connectURI);
+        throw e;
+    }
+};
